074d6af56974aee5e9fe46ea7ae8c1a47d992e13
fernandoGuedes <fernando.direitosnt@gmail.com>
2023-07-25 14:56:05 -0300

    package, readme and uppercase.js created

--- upperCase.js
+++ upperCase.js
@@ -1,19 +1,44 @@
-let fs = require('fs');
+const fs = require("fs");
+const path = require("path");
 
-let args = process.argv.slice(2);
+const inputFolder = "./entrada";
+const outputFolder = "./modificados";
 
-let fileName = args[0];
+if (!fs.existsSync(inputFolder)) {
+  fs.mkdirSync(inputFolder);
+  console.log(`Pasta ${inputFolder} criado.`);
+  console.log("coloque seus arquivos na pasta (entrada) e execute o codigo novamente.");
+}else{
 
-fs.readFile(fileName, "UTF8", (error, data) => {
-    if (error) throw error;
+  fs.readdir(inputFolder, (err, files) => {
+    if (err) {
+      console.error(err);
+      return;
+    }
+    if (!fs.existsSync(outputFolder)) {
+      fs.mkdirSync(outputFolder);
+      console.log(`Pasta ${outputFolder} criado.`);
+    };
+    files.forEach((file) => {
+      if (file.endsWith(".txt")) {
+        const inputFile = path.join(inputFolder, file);
+        fs.readFile(inputFile, "UTF8", (error, data) => {
+          if (error) throw error;
+  
+          const fileNameExtension = path.extname(file);
+          const fileNamesemExtension = path.basename(file, fileNameExtension);
+          const newFileName = `${fileNamesemExtension}_Uppercase ${fileNameExtension}`;
+  
+          const outputFile = path.join(outputFolder, newFileName);
+          fs.writeFile(outputFile, data.toUpperCase(), (error) => {
+            if (error) throw error;
+            console.log(`${newFileName} gerado com sucesso.`);
+          });
+        });
+      }
+    });
+  });
+  
 
-    fs.writeFile(fileName + "_Uppercase", data.toUpperCase(), (error) => {
-        if (error) throw error;
-
-        console.log("arquivo gerado com sucesso");
-    })
-})
-
-
-
+}
 
